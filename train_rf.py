39d47f3971fde3436962651c3fb12225d7ea9470
Hanlin He <hanling.he@gmail.com>
2017-11-28 16:11:10 -0600

    Add license.

--- train_rf.py
+++ train_rf.py
@@ -1,3 +1,35 @@
+#!/usr/bin/env python
+
+# MIT License
+#
+# Copyright (c) 2017 CS6375
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+__author__ = "Hanlin He (hxh160630), Mingze Xu (mxx160530), Su Yang (sxy161730) and Tao Wang (txw162630)"
+__copyright__ = "Copyright 2017, The CS6375 Project"
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Hanlin He"
+__email__ = "hxh160630@utdallas.edu"
+__status__ = "Development"
+
 from logging import StreamHandler, DEBUG, Formatter, FileHandler, getLogger
 
 import numpy as np
